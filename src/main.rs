4f982b6adb23f979db2a420f3e98a272b14439c9
Benign X <1341398182@qq.com>
2024-02-02 00:26:13 +0800

    🧪(test): update test

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,21 @@ mod tests {
     #[test]
     fn it_works() {
         let data = include_bytes!("../res/img_0.png");
-        let mid = MiData::decode_from::<PNG>(Vec::from(*data));
-        let data = mid.encode_into::<PNG>();
-        let mid = MiData::decode_from::<PNG>(data);
-        assert_eq!(mid.encode_into::<PNG>().len(), 172953);
+        let mid_before = MiData::decode_from::<PNG>(Vec::from(*data));
+        let data = mid_before.encode_into::<PNG>();
+        let mid_after = MiData::decode_from::<PNG>(data);
+
+        let image_buffer_before = match mid_before {
+            MiData::RGBA(img) => img,
+            MiData::GRAY(_) | MiData::PATH => panic!("Unexpected type"),
+        };
+
+        let image_buffer_after = match mid_after {
+            MiData::RGBA(img) => img,
+            MiData::GRAY(_) | MiData::PATH => panic!("Unexpected type"),
+        };
+
+        assert_eq!(image_buffer_before.width(), 285);
+        assert_eq!(image_buffer_before, image_buffer_after);
     }
 }
