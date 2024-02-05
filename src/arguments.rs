1c2f7816306b760ae4b910bed78cc10a42b20c85
Benign X <1341398182@qq.com>
2024-02-06 01:48:42 +0800

    ❤️(args): add sub-commands group and show commands

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[allow(non_camel_case_types)]
 #[derive(Debug, Clone, Copy, PartialOrd, PartialEq, ValueEnum)]
@@ -51,27 +51,48 @@ pub(crate) enum ImageFormats {
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
-#[command(about = "Convert image files to any other image format including LVGL image formats.")]
+#[command(
+    about = "`Show` or `Convert` image files to any other image format including LVGL image formats."
+)]
 pub struct Args {
-    /// input files
-    #[arg(short = 'i', long, required = true, value_hint = clap::ValueHint::FilePath)]
-    pub(crate) input_files: Vec<String>,
-
-    /// input image formats
-    #[arg(short = 'f', long, value_enum, default_value = "common")]
-    pub(crate) input_format: ImageFormatCategory,
-
-    /// output image formats
-    #[arg(short = 't', long, value_enum)]
-    pub(crate) output_format: ImageFormats,
-
-    /// lvgl version
-    #[arg(short = 'l', long, value_enum, default_value = "v9")]
-    pub(crate) lvgl_version: LVGL_Version,
+    #[command(subcommand)]
+    commands_show: CommandsShow,
 
     /// verbose mode
     #[arg(short = 'v', long, action = clap::ArgAction::Count)]
     pub(crate) verbose: u8,
+}
+
+#[derive(Subcommand, Debug)]
+pub(crate) enum CommandsShow {
+    /// Show an image file
+    Show {
+        /// an image file to show
+        file: String,
+
+        /// input image formats
+        #[arg(short = 'f', long, value_enum, default_value = "common")]
+        input_format: ImageFormatCategory,
+    },
+
+    /// Convert image files to any other image format including LVGL image formats.
+    Convert {
+        /// input files
+        #[arg(short = 'i', long, required = true, value_hint = clap::ValueHint::FilePath)]
+        input_files: Vec<String>,
+
+        /// input image formats
+        #[arg(short = 'f', long, value_enum, default_value = "common")]
+        input_format: ImageFormatCategory,
+
+        /// output image formats
+        #[arg(short = 't', long, value_enum)]
+        output_format: ImageFormats,
+
+        /// lvgl version
+        #[arg(short = 'l', long, value_enum, default_value = "v9")]
+        lvgl_version: LVGL_Version,
+    },
 }
 
 pub fn parse_args() -> Args {
