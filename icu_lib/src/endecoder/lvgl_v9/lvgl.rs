2c449baa1aa3a4303b1c1d30b288a079e4fbdd57
Benign X <1341398182@qq.com>
2024-03-03 23:47:33 +0800

    🐛(icu_lib): fix decode with and height revers problem

--- icu_lib/src/endecoder/lvgl_v9/lvgl.rs
+++ icu_lib/src/endecoder/lvgl_v9/lvgl.rs
@@ -72,8 +72,8 @@ impl EnDecoder for LVGL {
 
         // Convert image data to RGBA
         let img_buffer = RgbaImage::from_vec(
-            img_desc.header.h as u32,
             img_desc.header.w as u32,
+            img_desc.header.h as u32,
             rgba8888_from(
                 img_desc.data.clone().as_mut(),
                 header.cf,
