589aa16ac7b916fc7c8e8a0d902553893b8de25c
Benign X <1341398182@qq.com>
2024-12-02 18:32:33 +0800

    👩‍💻(ImageShower): improve dropped files handling
    Refactor the way dropped files are displayed and processed. Now, each file's information is more accurately represented, and image data is decoded and prepared for display if possible. The process includes checking file paths, names, and bytes, with a focus on extracting and displaying RGBA images.
    
    Signed-off-by: Benign X <1341398182@qq.com>

--- src/image_shower.rs
+++ src/image_shower.rs
@@ -253,35 +253,63 @@ impl MyEguiApp {
 
         // Show dropped files (if any):
         if !self.dropped_files.is_empty() {
-            let mut open = true;
-            egui::Window::new("Dropped files")
-                .open(&mut open)
-                .show(ctx, |ui| {
-                    for file in &self.dropped_files {
-                        let mut info = if let Some(path) = &file.path {
-                            path.display().to_string()
-                        } else if !file.name.is_empty() {
-                            file.name.clone()
+            for file in &self.dropped_files {
+                let info = if let Some(path) = &file.path {
+                    path.display().to_string()
+                } else if !file.name.is_empty() {
+                    file.name.clone()
+                } else {
+                    continue;
+                };
+
+                let mi_data = match &file.bytes {
+                    Some(bytes) => {
+                        if let Some(coder) = icu_lib::endecoder::find_endecoder(bytes) {
+                            coder.decode(bytes.to_vec())
                         } else {
-                            "???".to_owned()
-                        };
-
-                        let mut additional_info = vec![];
-                        if !file.mime.is_empty() {
-                            additional_info.push(format!("type: {}", file.mime));
+                            continue;
                         }
-                        if let Some(bytes) = &file.bytes {
-                            additional_info.push(format!("{} bytes", bytes.len()));
-                        }
-                        if !additional_info.is_empty() {
-                            info += &format!(" ({})", additional_info.join(", "));
-                        }
-
-                        ui.label(info);
                     }
-                });
-            if !open {
-                self.dropped_files.clear();
+                    None => {
+                        let data = std::fs::read(info);
+                        match data {
+                            Ok(data) => {
+                                if let Some(coder) = icu_lib::endecoder::find_endecoder(&data) {
+                                    coder.decode(data)
+                                } else {
+                                    continue;
+                                }
+                            }
+                            _ => continue,
+                        }
+                    }
+                };
+
+                match mi_data {
+                    MiData::RGBA(img_buffer) => {
+                        let width = img_buffer.width();
+                        let height = img_buffer.height();
+                        let image_data = Some(
+                            img_buffer
+                                .chunks(4)
+                                .map(|pixel| {
+                                    Color32::from_rgba_unmultiplied(
+                                        pixel[0], pixel[1], pixel[2], pixel[3],
+                                    )
+                                })
+                                .collect::<Vec<Color32>>(),
+                        );
+
+                        self.width = width;
+                        self.height = height;
+                        self.image_data = image_data;
+
+                        self.dropped_files.clear();
+                        break;
+                    }
+                    MiData::GRAY(_) => {}
+                    MiData::PATH => {}
+                };
             }
         }
     }
